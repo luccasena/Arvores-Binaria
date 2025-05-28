1f6caadfb1bda3eb5de1ef6ec1d6bb33a15f1439
Lucca de Sena Barbosa <luccasena22@gmail.com>
2025-05-28 17:44:49 -0300

    🚀Primeiro Commit: Adicionando script base.

---
+++ Principal.java
@@ -0,0 +1,69 @@
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String []args){
+        Scanner input = new Scanner(System.in);
+        utilidades util = new utilidades();
+        int escolha;
+
+
+        while(true){
+            try {
+                util.menu_principal();
+                escolha = input.nextInt();
+
+                if (escolha >= 0 || escolha <= 0){
+                    util.limpar_tela();
+                    switch(escolha){
+                        case 0:
+                            System.out.println("[0] -  Sair;");
+                            util.linhas();
+
+                            break;
+                        case 1:
+                            System.out.println("[1] -  Inserir Aluno;");
+                            util.linhas();
+
+                            break;
+                        case 2:
+                            System.out.println("[2] -  Remover Aluno;");
+                            util.linhas();
+
+                            break;
+                        case 3:
+                            System.out.println("[3] -  Pesquisar Aluno por RGM;");
+                            util.linhas();
+
+                            break;
+                        case 4:
+                            System.out.println("[4] -  Esvaziar;");
+                            util.linhas();
+
+                            break;
+                        case 5:
+                            System.out.println("[5] -  Exibir Árvore;");
+                            util.linhas();
+
+                            break;
+                        default:
+                            System.out.println("Limite de índices excedido! Tente novamente...");
+                            util.linhas();
+
+                    }
+                }
+                if(escolha == 0){
+                    break;
+                }
+            }catch (Exception e){
+                util.limpar_tela();
+                input.nextLine();
+                System.out.println("Opção Inválida! Tente novamente...");
+
+            }
+
+        }
+
+    }
+
+}
---
+++ utilidades.java
@@ -0,0 +1,36 @@
+public class utilidades {
+
+    public void limpar_tela(){
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    }
+
+    public void linhas(){
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-");
+    }
+
+    public void menu_principal(){
+        linhas();
+        System.out.println("            Árvore Binária para Cadastro de Alunos            ");
+        linhas();
+        System.out.println("[1] -  Inserir Aluno;");
+        System.out.println("[2] -  Remover Aluno;");
+        System.out.println("[3] -  Pesquisar Aluno por RGM;");
+        System.out.println("[4] -  Esvaziar;");
+        System.out.println("[5] -  Exibir Árvore;");
+        System.out.println("[0] -  Sair;");
+        linhas();
+    }
+
+    public void menu_exibir(){
+        linhas();
+        System.out.println("            Exibir Árvore Binária             ");
+        linhas();
+        System.out.println("[1] -  Pré-Ordem;");
+        System.out.println("[2] -  In-Ordem;");
+        System.out.println("[3] -  Pós-Ordem;");
+        System.out.println("[4] -  Graficamente;");
+        System.out.println("[0] -  Sair;");
+        linhas();
+    }
+
+}
